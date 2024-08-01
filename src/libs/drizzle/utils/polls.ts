ff10c2d7b6b2663c058f0e3af9b01a42431f4d9c
dawidseipold <seipold.dawid@gmail.com>
2024-08-02 00:19:20 +0200

    feat: add getAllPolls, createSinglePoll, getSinglePoll, deleteSinglePoll (for now mostly a copy of the quizzes functions) feat: add a support to sorting, ordering, limiting the amount of elements and offseting the list in the getAllPolls function

---
+++ src/libs/drizzle/utils/polls.ts
@@ -0,0 +1,84 @@
+import { asc, desc, eq } from "drizzle-orm";
+import { Database } from "../../../types";
+import { CustomError } from "../../utils/error";
+import { polls, type PollsSelect, type PollsInsert } from "../schemas";
+
+interface AllPollsQueryParams {
+  sort_by: keyof PollsSelect;
+  order_by: "asc" | "desc";
+  limit: number;
+  offset: number;
+}
+
+export const getAllPolls = async (
+  db: Database,
+  { sort_by, order_by, offset, limit }: AllPollsQueryParams
+) => {
+  const sortAndOrder = () => {
+    switch (order_by) {
+      case "asc":
+        return asc(polls[sort_by]);
+      case "desc":
+        return desc(polls[sort_by]);
+    }
+  };
+
+  const data = await db
+    .select()
+    .from(polls)
+    .orderBy(sortAndOrder())
+    .limit(limit)
+    .offset(offset);
+
+  if (data.length === 0) {
+    throw new CustomError(404, "Polls not found");
+  }
+
+  return {
+    name: "Success",
+    message: "Polls found",
+    data,
+  };
+};
+
+export const createSinglePoll = async (db: Database, title: string) => {
+  const data = await db.insert(polls).values({ title });
+
+  if (data[0].affectedRows === 0) {
+    throw new CustomError(500, "Failed to create poll");
+  }
+
+  return {
+    name: "Success",
+    message: "Poll created successfully",
+    data,
+  };
+};
+
+export const getSinglePoll = async (db: Database, id: number) => {
+  const data = await db.select().from(polls).where(eq(polls.id, id));
+
+  if (data.length === 0) {
+    throw new CustomError(404, "Poll not found");
+  }
+
+  return {
+    name: "Success",
+    message: "Poll found",
+    data,
+  };
+};
+
+export const deleteSinglePoll = async (db: Database, id: number) => {
+  const data = await db.delete(polls).where(eq(polls.id, id));
+
+  if (data[0].affectedRows === 0) {
+    throw new CustomError(404, "Poll not found");
+  }
+
+  return {
+    name: "Success",
+    message: "Poll deleted successfully",
+    data,
+  };
+};
