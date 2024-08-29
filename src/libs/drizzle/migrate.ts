aeedb4ba259d3dbd7f2ca1c4f8b96fe2f4518f3d
dawidseipold <seipold.dawid@gmail.com>
2024-08-29 23:18:57 +0200

    feat: add ability to exit itself to db:migrate script and add informational console.log

--- src/libs/drizzle/migrate.ts
+++ src/libs/drizzle/migrate.ts
@@ -2,5 +2,8 @@ import { dbClient, clientConnection } from ".";
 import { migrate } from "drizzle-orm/mysql2/migrator";
 
 await migrate(dbClient, { migrationsFolder: ".drizzle" });
-
 await clientConnection.end();
+
+console.log("Migration complete");
+
+process.exit();
