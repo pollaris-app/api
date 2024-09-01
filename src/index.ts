b6d2e9c0422c0a14bddbf8749d2717c0003a9440
dawidseipold <seipold.dawid@gmail.com>
2024-09-02 01:03:37 +0200

    feat: use recently created routes in the api v1

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import { Elysia } from "elysia";
+import { treaty } from "@elysiajs/eden";
+
 import { quizzesRoutes } from "./routes/quizzes";
 import { pollsRoutes } from "./routes/polls";
 import { usersRoutes } from "./routes/users";
-import { treaty } from "@elysiajs/eden";
+import { authRoutes } from "./routes/auth";
+import { emailsRoutes } from "./routes/emails";
 
 const app = new Elysia({ prefix: "/api/v1" })
+  .use(authRoutes)
+  .use(emailsRoutes)
+  .use(usersRoutes)
   .use(quizzesRoutes)
   .use(pollsRoutes)
-  .use(usersRoutes)
   .listen(3000);
 
 export type App = typeof app;
