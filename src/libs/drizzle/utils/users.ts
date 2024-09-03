a234bc4c1a94ee7c297c4bdf7c2d166ff87907ba
dawidseipold <seipold.dawid@gmail.com>
2024-09-03 22:06:46 +0200

    refactor: checkUserExists no longer returns errors and instead gives back the boolean feat: add getUserIdByEmail function feat: add patchPasswordHash function

--- src/libs/drizzle/utils/users.ts
+++ src/libs/drizzle/utils/users.ts
@@ -5,23 +5,25 @@ import { users } from "../schemas";
 import { Resend } from "resend";
 
 export const checkUserExists = async (db: Database, email: string) => {
-  try {
-    const data = await db.select().from(users).where(eq(users.email, email));
+  const data = await db
+    .select({
+      email: users.email,
+    })
+    .from(users)
+    .where(eq(users.email, email));
 
-    if (data.length > 0) {
-      throw new CustomError(409, "Email is already in use");
-    }
+  return data.length > 0;
+};
 
-    return {
-      name: "Success",
-      message: "Email is available",
-      data,
-    };
-  } catch (error) {
-    if (error instanceof CustomError) {
-      throw new CustomError(error.status, error.message);
-    }
-  }
+export const getUserIdByEmail = async (db: Database, email: string) => {
+  const data = await db
+    .select({
+      id: users.id,
+    })
+    .from(users)
+    .where(eq(users.email, email));
+
+  return data[0].id;
 };
 
 export const createSingleUser = async (
@@ -38,6 +40,27 @@ export const createSingleUser = async (
   return {
     name: "Success",
     message: "User created successfully",
+    data,
+  };
+};
+
+export const patchPasswordHash = async (
+  db: Database,
+  email: string,
+  passwordHash: string
+) => {
+  const data = await db
+    .update(users)
+    .set({ passwordHash })
+    .where(eq(users.email, email));
+
+  if (data[0].affectedRows === 0) {
+    throw new CustomError(500, "Failed to update password");
+  }
+
+  return {
+    name: "Success",
+    message: "Password updated successfully",
     data,
   };
 };
@@ -66,6 +89,29 @@ export const sendEmailVerificationEmail = async (
   return {
     name: "Success",
     message: "Verification email sent",
+    data,
+  };
+};
+
+export const sendPasswordResetEmail = async (email: string, token: string) => {
+  const resend = new Resend(Bun.env.RESEND_API_KEY);
+
+  const { data, error } = await resend.emails.send({
+    from: `Acme <onboarding@${Bun.env.RESEND_DOMAIN}>`,
+    to: [email],
+    subject: "Quizzly - Password Reset",
+    html: `
+      <a href="http://${Bun.env.HOST}/auth/password-reset/${token}">Reset</a>
+    `,
+  });
+
+  if (error) {
+    throw new CustomError(500, error.message);
+  }
+
+  return {
+    name: "Success",
+    message: "Password reset email sent",
     data,
   };
 };
