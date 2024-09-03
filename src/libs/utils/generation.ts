747b7f624933d5461d888e5e7f675a948e14b6d1
dawidseipold <seipold.dawid@gmail.com>
2024-09-03 22:07:11 +0200

    chore: update the name of generateVerificationToken function to generateToken

--- src/libs/utils/generation.ts
+++ src/libs/utils/generation.ts
@@ -2,7 +2,7 @@ export const generateVerificationCode = () => {
   return Math.floor(100000 + Math.random() * 900000).toString();
 };
 
-export const generateVerificationToken = () => {
+export const generateToken = () => {
   const hasher = new Bun.CryptoHasher("sha256");
 
   hasher.update(Math.random().toString(36).substring(2));
