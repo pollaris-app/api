d54460b91079f088bf68823a39c0ff8c2b07fd4c
dawidseipold <seipold.dawid@gmail.com>
2024-09-03 22:08:41 +0200

    feat: add route for password reset email

--- src/routes/emails/index.ts
+++ src/routes/emails/index.ts
@@ -1,27 +1,51 @@
 import Elysia, { t } from "elysia";
-import { sendEmailVerificationEmail } from "../../libs/drizzle/utils/users";
+import {
+  sendEmailVerificationEmail,
+  sendPasswordResetEmail,
+} from "../../libs/drizzle/utils/users";
 import { CustomError, handleError } from "../../libs/utils/error";
 
 export const emailsRoutes = new Elysia({
   prefix: "/emails",
-}).post(
-  "/email-verification",
-  async ({ body: { email, token, code } }) => {
-    try {
-      const { data } = await sendEmailVerificationEmail(email, token, code);
+})
+  .post(
+    "/email-verification",
+    async ({ body: { email, token, code } }) => {
+      try {
+        const { data } = await sendEmailVerificationEmail(email, token, code);
 
-      return data;
-    } catch (error) {
-      if (error instanceof CustomError) {
-        return handleError(error);
+        return data;
+      } catch (error) {
+        if (error instanceof CustomError) {
+          return handleError(error);
+        }
       }
+    },
+    {
+      body: t.Object({
+        email: t.String(),
+        token: t.String(),
+        code: t.String(),
+      }),
     }
-  },
-  {
-    body: t.Object({
-      email: t.String(),
-      token: t.String(),
-      code: t.String(),
-    }),
-  }
-);
+  )
+  .post(
+    "/password-reset",
+    async ({ body: { email, token } }) => {
+      try {
+        const { data } = await sendPasswordResetEmail(email, token);
+
+        return data;
+      } catch (error) {
+        if (error instanceof CustomError) {
+          return handleError(error);
+        }
+      }
+    },
+    {
+      body: t.Object({
+        email: t.String(),
+        token: t.String(),
+      }),
+    }
+  );
