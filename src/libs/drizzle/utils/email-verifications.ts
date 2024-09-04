06226abdb1fc7d4cf970c01572314c151e4fa19f
dawidseipold <seipold.dawid@gmail.com>
2024-09-05 01:12:57 +0200

    feat: add createEmailVerification function

--- src/libs/drizzle/utils/email-verifications.ts
+++ src/libs/drizzle/utils/email-verifications.ts
@@ -1,6 +1,7 @@
 import { and, eq } from "drizzle-orm";
 import { Database } from "../../../types";
 import { emailVerifications } from "../schemas";
+import { CustomError } from "../../utils/error";
 
 export const checkIfEmailVerificationExists = async (
   db: Database,
@@ -47,4 +48,25 @@ export const validateEmailVerification = async (
     );
 
   return verification.length > 0;
+};
+
+export const createEmailVerification = async (
+  db: Database,
+  userId: number,
+  token: string,
+  code: string
+) => {
+  const data = await db
+    .insert(emailVerifications)
+    .values({ userId, token, code, expiresAt: new Date() });
+
+  if (data[0].affectedRows === 0) {
+    throw new CustomError(500, "Failed to create email verification");
+  }
+
+  return {
+    name: "Success",
+    message: "Email verification created successfully",
+    data,
+  };
 };
